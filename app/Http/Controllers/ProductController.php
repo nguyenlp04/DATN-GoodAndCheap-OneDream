ef56c261e80b629bcfb6a9358d2297bfa35aeebf
nguyenlp04 <lphuonguye.ecn.2182004@gmail.com>
2024-11-11 09:43:58 +0700

    update json data product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -90,15 +90,7 @@ class ProductController extends Controller
                 'dataVariantDetail' => 'required',  // Đảm bảo là mảng
                 'variant' => 'required',  // Đảm bảo là mảng
             ]);
-            
-            // dd($request->input('variant'), $request->input('dataVariantDetail'));
-            // Tạo mảng con dataVariant với cấu trúc đúng
-            $dataVariant = [
-                'variants' => $validatedData['variant'],  // Lấy giá trị từ 'Variant' trong validatedData
-                'variant_data_details' => $validatedData['dataVariantDetail']  // Lấy giá trị từ 'dataVariantDetail'
-            ];
-            
-            // Tạo mảng $data với các thông tin khác
+           
             $data = [
                 'productTitle' => $validatedData['productTitle'],
                 'price' => $validatedData['price'],
@@ -107,7 +99,8 @@ class ProductController extends Controller
                 'category_id' => $validatedData['category_id'],
                 'subcategory_id' => $validatedData['subcategory_id'],
                 'description' => $validatedData['description'],
-                'dataVariant' => $dataVariant,  // Gán mảng con vào dataVariant
+                'variants' => $validatedData['variant'],
+                'variant_data_details' => $validatedData['dataVariantDetail']
             ];
             
             
