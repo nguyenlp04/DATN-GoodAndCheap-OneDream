6458ee60371df447fb11662d3f1832b64f6a3026
sgswebvn <hieucv204@gmail.com>
2024-11-18 20:42:42 +0700

    merge main

--- app/Http/Controllers/SaleNewsController.php
+++ app/Http/Controllers/SaleNewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
