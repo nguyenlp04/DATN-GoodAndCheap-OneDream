3959db1b828e72facf8c0718fc087ba54c4902e3
sgswebvn <hieucv204@gmail.com>
2024-12-08 19:58:03 +0700

    fix notification

--- resources/views/blocks/client/header.blade.php
+++ resources/views/blocks/client/header.blade.php
@@ -12,7 +12,7 @@
    }
 
    .main-nav {
-      
+      width: 100%;
 
       white-space: nowrap;
       overflow: hidden;
@@ -116,41 +116,45 @@
                       <p>notifications</p>
                   </a>
                   <div class="dropdown-menu-right">
-                      @if (!empty($notifications))
-                          <ul class="dropdown-menu dropdown-menu-end p-0 show border" data-bs-popper="static" style="border: 1px solid #ddd; border-radius: 5px;">
-                              <li class="dropdown-notifications-list scrollable-container" style="max-height: 200px; overflow-y: auto;">
-                                  <ul class="list-group list-group-flush">
-                                      @for ($i = 0; $i < count($notifications); $i++)
-                                          <a href="{{ route('notifications.detail', ['notification' => $notifications[$i]['notification_id']]) }}" class="notification-link">
-                                              <li class="list-group-item list-group-item-action dropdown-notifications-item marked-as-read border-bottom" style="border-bottom: 1px solid #ddd;">
-                                                  <div class="d-flex">
-                                                      <div class="flex-grow-1">
-                                                          <h6 class="mb-1">{{ Str::limit($notifications[$i]['title_notification'], 27) }}</h6>
-                                                          <h5 class="mb-0">{!! Str::limit($notifications[$i]['content_notification'], 40) !!}</h5>
-                                                          @php
-                                                          $createdAt = new DateTime($notifications[$i]['created_at']);
-                                                          @endphp
-                                                          <small class="text-muted">{{ $createdAt->format('d/m/Y H:i') }}</small>
-                                                      </div>
-                                                  </div>
-                                              </li>
-                                          </a>
-                                      @endfor
-                                  </ul>
-                              </li>
-                              <li class="border-top">
-                                  <div class="d-grid p-4">
-                                      <a class="btn btn-primary" href="{{ route('notifications.show') }}">
-                                          <small >View all notifications</small>
-                                      </a>
-                                  </div>
-                              </li>
-                          </ul>
-                      @else
-                          <p class="text-center" style="border: 1px solid #ddd; padding: 10px; border-radius: 5px;">No notifications available.</p>
-                      @endif
-                  </div>
-              </div>
+                     @if (!empty($notifications))
+                         <ul class="dropdown-menu dropdown-menu-end p-0 show border" data-bs-popper="static" style="border: 1px solid #ddd; border-radius: 5px;">
+                             <li class="dropdown-notifications-list scrollable-container" style="max-height: 200px; overflow-y: auto;">
+                                 <ul class="list-group list-group-flush">
+                                     @for ($i = 0; $i < count($notifications); $i++)
+                                         <a href="{{ route('notifications.detail', ['notification' => $notifications[$i]['notification_id']]) }}" class="notification-link">
+                                             <li class="list-group-item list-group-item-action dropdown-notifications-item marked-as-read border-bottom" style="border-bottom: 1px solid #ddd;">
+                                                 <div class="d-flex">
+                                                     <div class="flex-grow-1">
+                                                         <h6 class="mb-1">{{ Str::limit($notifications[$i]['title_notification'], 27) }}</h6>
+                                                         <h5 class="mb-0">{!! Str::limit($notifications[$i]['content_notification'], 40) !!}</h5>
+                                                         @php
+                                                         $createdAt = new DateTime($notifications[$i]['created_at']);
+                                                         @endphp
+                                                         <small class="text-muted">{{ $createdAt->format('d/m/Y H:i') }}</small>
+                                                     </div>
+                                                 </div>
+                                             </li>
+                                         </a>
+                                     @endfor
+                                 </ul>
+                             </li>
+                             <li class="border-top">
+                                 <div class="d-grid p-4">
+                                     <a class="btn btn-primary" href="{{ route('notifications.show') }}">
+                                         <small>View all notifications</small>
+                                     </a>
+                                 </div>
+                             </li>
+                         </ul>
+                     @else
+                         <ul class="dropdown-menu dropdown-menu-end p-0 show border" data-bs-popper="static" style="border: 1px solid #ddd; border-radius: 5px;">
+                             <li class="dropdown-notifications-list text-center p-3">
+                                 <span class="text-muted">No notifications available.</span>
+                             </li>
+                         </ul>
+                     @endif
+                 </div>
+                 
                  
             @else
             <div class="wishlist">
